a134107cc415b2415849ac1e601f60e92e761e9a
Kai Zhang <zhangkai7@xiamenair.com>
2019-10-11 12:06:25 +0800

    初始提交

---
+++ Chapter08/16_typeofOperator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace _8._16_typeofOperator
+{
+    class SomeClass
+    {
+        public int Field1;
+        public int Field2;
+
+        public void Method1() { }
+        public int Method2() { return 1; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(SomeClass);
+            FieldInfo[] fi = t.GetFields();
+            MethodInfo[] mi = t.GetMethods();
+
+            foreach (FieldInfo f in fi)
+            {
+                Console.WriteLine("Field : {0}", f.Name);
+            }
+            foreach (MethodInfo m in mi)
+            {
+                Console.WriteLine("Method : {0}", m.Name);
+            }
+
+            SomeClass s = new SomeClass();
+
+            Console.WriteLine("Type s: {0}", s.GetType().Name);
+        }
+    }
+}
